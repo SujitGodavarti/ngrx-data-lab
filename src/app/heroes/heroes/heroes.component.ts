b098fed6437a8330cc45bff68fedaa2ca4e418f5
sgodavarti <sgodavarti@pceftpos.com.au>
2018-10-09 08:16:12 +0530

    first commit

---
+++ src/app/heroes/heroes/heroes.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { Hero } from '../../core';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  selected: Hero;
+  heroes$: Observable<Hero[]>;
+  loading: boolean;
+
+  constructor(private heroService: HeroService) {
+    this.heroes$ = heroService.entities$;
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  close() {
+    this.selected = null;
+  }
+
+  enableAddMode() {
+    this.selected = <any>{};
+  }
+
+  select(hero: Hero) {
+    this.selected = hero;
+  }
+
+  add(hero: Hero) {
+    this.heroService.add(hero);
+  }
+  
+  delete(hero: Hero) {
+    this.heroService.delete(hero);
+    this.close();
+  }
+  
+  getHeroes() {
+    this.heroService.getAll();
+    this.close();
+  }
+  
+  update(hero: Hero) {
+    this.heroService.update(hero);
+  }
+}
